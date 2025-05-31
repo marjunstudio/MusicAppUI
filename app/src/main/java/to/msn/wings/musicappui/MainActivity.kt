75fff08d9169c256acbdead014c7349fb74d9872
marjunstudio <uebujie@gmail.com>
2025-05-31 09:21:32 +0900

    first commit

---
+++ app/src/main/java/to/msn/wings/musicappui/MainActivity.kt
@@ -0,0 +1,26 @@
+package to.msn.wings.musicappui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import to.msn.wings.musicappui.ui.theme.MusicAppUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MusicAppUITheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
