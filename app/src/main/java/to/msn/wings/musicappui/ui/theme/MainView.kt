7f3e418865d97e720bbc2edac9e525a86c56b392
marjunstudio <uebujie@gmail.com>
2025-05-31 13:13:30 +0900

    Add Account画面のビューを作成

--- app/src/main/java/to/msn/wings/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/to/msn/wings/musicappui/ui/theme/MainView.kt
@@ -67,7 +67,7 @@ fun MainView() {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Home") },
+            TopAppBar(title = { Text(title.value) },
                 navigationIcon = { IconButton(onClick = {
                     // コルーチン処理を行うことでUIブロックを防ぐ
                     scope.launch {
@@ -91,8 +91,8 @@ fun MainView() {
                             dialogOpen.value = true
                         } else {
                             controller.navigate(item.dRoute)
-                            title.value = item.dTitle
                         }
+                        title.value = item.dTitle
                     }
                 }
             }
@@ -133,10 +133,10 @@ fun DrawerItem(
 @Composable
 fun Navigation(navController: NavController, viewModel: MainViewModel, pd: PaddingValues) {
     NavHost(navController = navController as NavHostController,
-        startDestination = Screen.DrawerScreen.AddAccount.route, modifier = Modifier.padding(pd)) {
+        startDestination = Screen.DrawerScreen.Account.route, modifier = Modifier.padding(pd)) {
 
-        composable(Screen.DrawerScreen.AddAccount.route) {
-
+        composable(Screen.DrawerScreen.Account.route) {
+            AccountView()
         }
         composable(Screen.DrawerScreen.Subscription.route) {
 
